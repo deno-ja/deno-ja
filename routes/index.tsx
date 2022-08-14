79e9e60d611061939976d3dce3783b2ea720a92a
hashrock <hashrock@users.noreply.github.com>
2022-08-14 19:25:02 +0900

    add links to the uki's book and slack info

--- routes/index.tsx
+++ routes/index.tsx
@@ -19,6 +19,20 @@ export default function Home() {
           Deno Japan Users Group
         </p>
       </div>
+      <div class={tw`mt-12 mx-auto flex max-w-screen-sm text-center gap-6`}>
+          <a
+            class={tw`block flex-1 max-w- px-6 py-3 no-underline bg-green-100 border-2 border-black rounded hover:bg-green-200 hover:underline shadow-md`}
+            href="https://zenn.dev/uki00a/books/effective-deno/viewer/what-is-deno"
+          >
+            Deno とは？
+          </a>
+          <a
+            class={tw`block flex-1 px-6 py-3 no-underline bg-white border-2 border-black rounded hover:bg-gray-100 hover:underline shadow-md`}
+            href="https://scrapbox.io/deno-ja/Slack%E3%81%AE%E5%8F%82%E5%8A%A0%E6%96%B9%E6%B3%95"
+          >
+            Slack に参加する
+          </a>
+        </div>
 
       <div>
         {latest &&
