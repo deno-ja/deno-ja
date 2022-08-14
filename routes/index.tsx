e6b9dee92075bd9b321f61e99c15bb6ecf509756
hashrock <hashrock@users.noreply.github.com>
2022-08-14 20:38:55 +0900

    split

--- routes/index.tsx
+++ routes/index.tsx
@@ -11,115 +11,133 @@ const toDateStr = (date: Date) => {
   return format(date, "yyyy-MM-dd HH:mm");
 };
 
+function Introduction() {
+  return (
+    <div class={tw`flex flex-col items-center mt-20`}>
+      <img src="/denoja-logo.svg" class={tw`w-80`} alt="deno-jaのロゴ" />
+      <p class={tw`text-xl text-gray-700`}>
+        Deno Japan Users Group
+      </p>
+
+      <p class={tw`mt-32 text-gray-700`}>
+        deno-jaは、Denoの日本ユーザによるオンラインコミュニティです。<br />
+        主にSlack上で情報共有や雑談などの交流を行なっています。<br />
+        また、月に一回「<a
+          href="https://deno-ja.connpass.com/"
+          class={tw`font-bold text-center hover:text-underline`}
+        >
+          Denoばた会議
+        </a>」というオンラインイベントを行なっています。<br />
+        どなたでも参加できますので、気軽に覗いてみてください。<br />
+      </p>
+
+      <div class={tw`mt-12 flex text-center gap-6`}>
+        <a
+          class={tw`block w-48 px-6 py-3 no-underline bg-green-100 border-2 border-black rounded hover:bg-green-200 hover:underline shadow-md`}
+          href="https://zenn.dev/uki00a/books/effective-deno/viewer/what-is-deno"
+        >
+          Deno とは？
+        </a>
+        <a
+          class={tw`block w-48 px-6 py-3 no-underline bg-white border-2 border-black rounded hover:bg-gray-100 hover:underline shadow-md`}
+          href="https://scrapbox.io/deno-ja/Slack%E3%81%AE%E5%8F%82%E5%8A%A0%E6%96%B9%E6%B3%95"
+        >
+          Slack に参加する
+        </a>
+      </div>
+    </div>
+  );
+}
+
+function Denobata() {
+  return (
+    <div>
+      {latest &&
+        (
+          <div class={tw`flex items-center`}>
+            <div class={tw``}>
+              <a href={latest.event_url} tabIndex={-1}>
+                <img
+                  class={tw`w-full`}
+                  src="/denobata-title.png"
+                  alt="denoばた会議タイトルロゴ"
+                />
+              </a>
+              <a href={latest.event_url}>
+                <h2
+                  class={tw`text-xl font-bold text-center hover:text-underline`}
+                >
+                  {latest.title}
+                </h2>
+              </a>
+              <p class={tw`text-center`}>
+                {toDateStr(new Date(latest.started_at))}〜
+              </p>
+            </div>
+            <div>
+              <img
+                class={tw`w-full`}
+                src="/denobata-illust.png"
+                alt="denoばた会議イメージ図"
+              />
+            </div>
+          </div>
+        )}
+    </div>
+  );
+}
+
+function Contents() {
+  return (
+    <div class={tw`bg-gray-100 p-4 mt-20`}>
+      <h2 class={tw`text-3xl text-center mt-10 text-gray-400`}>
+        Contents by deno-ja
+      </h2>
+
+      <div class={tw`p-4 max-w-screen-md mx-auto space-y-10 mt-10`}>
+        {contents.map((content) => (
+          <div class={tw`flex gap-4`}>
+            <div
+              class={tw`inline-block p-3 rounded-full h-12 w-12 ${content.class}`}
+            >
+              <content.icon />
+            </div>
+            <div>
+              <div>
+                <a
+                  href={content.href}
+                  class={tw`font-bold hover:text-underline`}
+                >
+                  {content.title}
+                </a>{" "}
+                by{" "}
+                <a
+                  href={content.authorUrl}
+                  class={tw`hover:text-underline`}
+                >
+                  {content.author}
+                </a>
+              </div>
+              <div class={tw`pt-2 text-gray-800`}>
+                {content.description}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
 export default function Home() {
   return (
     <div>
       <div class={tw`p-4 mx-auto max-w-screen-md space-y-32`}>
-        <div class={tw`flex flex-col items-center mt-20`}>
-          <img src="/denoja-logo.svg" class={tw`w-80`} alt="deno-jaのロゴ" />
-          <p class={tw`text-xl text-gray-700`}>
-            Deno Japan Users Group
-          </p>
-
-          <p class={tw`mt-32 text-gray-700`}>
-            deno-jaは、Denoの日本ユーザによるオンラインコミュニティです。<br />
-            主にSlack上で情報共有や雑談などの交流を行なっています。<br />
-            また、月に一回「<a
-              href="https://deno-ja.connpass.com/"
-              class={tw`font-bold text-center hover:text-underline`}
-            >
-              Denoばた会議
-            </a>」というオンラインイベントを行なっています。<br />
-            どなたでも参加できますので、気軽に覗いてみてください。<br />
-          </p>
-
-          <div class={tw`mt-12 flex text-center gap-6`}>
-            <a
-              class={tw`block w-48 px-6 py-3 no-underline bg-green-100 border-2 border-black rounded hover:bg-green-200 hover:underline shadow-md`}
-              href="https://zenn.dev/uki00a/books/effective-deno/viewer/what-is-deno"
-            >
-              Deno とは？
-            </a>
-            <a
-              class={tw`block w-48 px-6 py-3 no-underline bg-white border-2 border-black rounded hover:bg-gray-100 hover:underline shadow-md`}
-              href="https://scrapbox.io/deno-ja/Slack%E3%81%AE%E5%8F%82%E5%8A%A0%E6%96%B9%E6%B3%95"
-            >
-              Slack に参加する
-            </a>
-          </div>
-        </div>
-
-        <div>
-          {latest &&
-            (
-              <div class={tw`flex items-center`}>
-                <div class={tw``}>
-                  <a href={latest.event_url} tabIndex={-1}>
-                    <img
-                      class={tw`w-full`}
-                      src="/denobata-title.png"
-                      alt="denoばた会議タイトルロゴ"
-                    />
-                  </a>
-                  <a href={latest.event_url}>
-                    <h2
-                      class={tw`text-xl font-bold text-center hover:text-underline`}
-                    >
-                      {latest.title}
-                    </h2>
-                  </a>
-                  <p class={tw`text-center`}>
-                    {toDateStr(new Date(latest.started_at))}〜
-                  </p>
-                </div>
-                <div>
-                  <img
-                    class={tw`w-full`}
-                    src="/denobata-illust.png"
-                    alt="denoばた会議イメージ図"
-                  />
-                </div>
-              </div>
-            )}
-        </div>
+        <Introduction />
+        <Denobata />
       </div>
-      <div class={tw`bg-gray-100 p-4 mt-20`}>
-        <h2 class={tw`text-3xl text-center mt-10 text-gray-400`}>
-          Contents by deno-ja
-        </h2>
 
-        <div class={tw`p-4 max-w-screen-md mx-auto space-y-10 mt-10`}>
-          {contents.map((content) => (
-            <div class={tw`flex gap-4`}>
-              <div
-                class={tw`inline-block p-3 rounded-full h-12 w-12 ${content.class}`}
-              >
-                <content.icon />
-              </div>
-              <div>
-                <div>
-                  <a
-                    href={content.href}
-                    class={tw`font-bold hover:text-underline`}
-                  >
-                    {content.title}
-                  </a>{" "}
-                  by{" "}
-                  <a
-                    href={content.authorUrl}
-                    class={tw`hover:text-underline`}
-                  >
-                    {content.author}
-                  </a>
-                </div>
-                <div class={tw`pt-2 text-gray-800`}>
-                  {content.description}
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
+      <Contents />
 
       <footer class={tw`p-12 max-w-screen-md mx-auto text-center`}>
         <ul class={tw`flex gap-4 justify-center`}>
