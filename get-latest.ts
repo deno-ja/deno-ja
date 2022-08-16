11bcc18d6a225e6e442af9dd32ea83022076f736
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-08-16 09:47:43 -0500

    no any
    little refactoring, `json` was `any`

--- get-latest.ts
+++ get-latest.ts
@@ -1,25 +1,23 @@
-const json = await (await fetch("https://connpass.com/api/v1/event/?series_id=7931")).json()
+const json: { events: Event[] } = await(
+  await fetch("https://connpass.com/api/v1/event/?series_id=7931")
+).json();
 
 interface Event {
-  event_id: number
-  title: string
-  catch: string
-  started_at: string
-  ended_at: string
-  limit: number
-  event_type: string
-  accepted: number
-  waiting: number
-  updated_at: string
-  event_url: string
-  place: string
-  address: string
+  event_id: number;
+  title: string;
+  catch: string;
+  started_at: string;
+  ended_at: string;
+  limit: number;
+  event_type: string;
+  accepted: number;
+  waiting: number;
+  updated_at: string;
+  event_url: string;
+  place: string;
+  address: string;
 }
 
-const latest: Event = json.events[0]
+const [latest] = json.events;
 
-console.log(
-  latest.title,
-  latest.event_url,
-  latest.started_at
-)
+console.log(latest.title, latest.event_url, latest.started_at);
