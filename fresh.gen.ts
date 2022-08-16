c060690d08fb63250173a9cb26da158b43a93fdf
hashrock <hashrock@users.noreply.github.com>
2022-08-17 00:05:01 +0900

    remove unused files

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -2,22 +2,15 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $0 from "./routes/[name].tsx";
-import * as $1 from "./routes/api/events.ts";
-import * as $2 from "./routes/api/joke.ts";
-import * as $3 from "./routes/index.tsx";
-import * as $$0 from "./islands/Counter.tsx";
+import * as $0 from "./routes/api/events.ts";
+import * as $1 from "./routes/index.tsx";
 
 const manifest = {
   routes: {
-    "./routes/[name].tsx": $0,
-    "./routes/api/events.ts": $1,
-    "./routes/api/joke.ts": $2,
-    "./routes/index.tsx": $3,
+    "./routes/api/events.ts": $0,
+    "./routes/index.tsx": $1,
   },
-  islands: {
-    "./islands/Counter.tsx": $$0,
-  },
+  islands: {},
   baseUrl: import.meta.url,
 };
 
