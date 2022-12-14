f802d5eaa1933a9033907f3c1c4b091e609c26c0
hashrock <hashrock@users.noreply.github.com>
2022-12-14 18:34:08 +0900

    fix: set ja to lang

--- main.ts
+++ main.ts
@@ -10,4 +10,10 @@ import manifest from "./fresh.gen.ts";
 import twindPlugin from "$fresh/plugins/twind.ts";
 import twindConfig from "./twind.config.ts";
 
-await start(manifest, { plugins: [twindPlugin(twindConfig)] });
+await start(manifest, {
+  plugins: [twindPlugin(twindConfig)],
+  render: (ctx, render) => {
+    ctx.lang = "ja";
+    render();
+  },
+});
