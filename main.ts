01b9feac9cad3c21ede0940e3d42518536ddf6d5
Yuki Tanaka <uki00a@gmail.com>
2023-08-28 03:40:52 +0900

    chore: introduce `fresh.config.ts` (#17)

--- main.ts
+++ main.ts
@@ -6,10 +6,6 @@
 
 import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
+import config from "./fresh.config.ts";
 
-import twindPlugin from "$fresh/plugins/twind.ts";
-import twindConfig from "./twind.config.ts";
-
-await start(manifest, {
-  plugins: [twindPlugin(twindConfig)],
-});
+await start(manifest, config);
